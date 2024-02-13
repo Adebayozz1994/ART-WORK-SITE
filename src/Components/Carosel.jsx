f48a27e0fddd6b2a08b0ed5bcde926cdf0ae845e
ogunlade adebayo peter <ogunladeadebayopeter@gmail.com>
2024-02-13 11:21:07 -0100

    new commit

--- src/Components/Carosel.jsx
+++ src/Components/Carosel.jsx
@@ -1,24 +1,38 @@
-import React from 'react'
-import { Carousel } from 'flowbite-react'
+import React from 'react';
+import { Carousel } from 'flowbite-react';
+import postcard from '../assets/Image/postcard.jpg';
+import Banners from '../assets/Image/Banners.png';
+import printivo from '../assets/Image/printivo.png';
+import printing from '../assets/Image/printing.png';
 
 const Carosel = () => {
   return (
-    <>
-         <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
-      <Carousel>
-        <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-          Slide 1
+    <div className="flex flex-wrap">
+      <section className="w-full lg:w-1/2">
+        <div className="h-96">
+          <Carousel>
+            <div className="flex flex-col h-full items-center justify-center bg-white-100 dark:bg-white-100 dark:text-white">
+              <img src={postcard} alt="" className="w-full h-48 object-cover" />
+              <p className="mt-2 text-red-700">Postcard Description</p>
+            </div>
+            <div className="flex flex-col h-full items-center justify-center bg-white-100 dark:bg-white-100 dark:text-white">
+              <img src={Banners} alt="" className="w-full h-48 object-cover" />
+              <p className="mt-2 text-red-700">Banners Description</p>
+            </div>
+            <div className="flex flex-col h-full items-center justify-center bg-white-100 dark:bg-white-100 dark:text-white">
+              <img src={printivo} alt="" className="w-full h-48 object-cover" />
+              <p className="mt-2 text-red-700">Printivo Description</p>
+            </div>
+          </Carousel>
         </div>
-        <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-          Slide 2
+      </section>
+      <section className="w-full lg:w-1/2">
+        <div className="flex justify-center items-center h-full">
+          <img src={printing} alt="" />
         </div>
-        <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-          Slide 3
-        </div>
-      </Carousel>
+      </section>
     </div>
-    </>
-  )
-}
+  );
+};
 
-export default Carosel
+export default Carosel;
